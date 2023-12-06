905507ef71bfecc5500d91d6c670cf9f4c99ee2e
Madan <141886991+madan6557@users.noreply.github.com>
2023-12-06 18:28:49 +0800

    Update gcs.js

--- Backend/src/gcs.js
+++ Backend/src/gcs.js
@@ -1,9 +1,8 @@
-// src/gcs.js
 const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage({
-  projectId: 'your-project-id', // Ganti dengan ID proyek Google Cloud Anda
-  keyFilename: 'path/to/your/keyfile.json', // Ganti dengan path ke file kunci JSON GCP Anda
+  projectId: 'your-project-id', 
+  keyFilename: 'path/to/your/keyfile.json',
 });
 
 module.exports = storage;
