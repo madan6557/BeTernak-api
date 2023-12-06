fa3734685465e4636ea856bbc30c1803da3f96ba
Madan <141886991+madan6557@users.noreply.github.com>
2023-12-06 18:30:48 +0800

    Update mlConnection.js

--- Backend/src/mlConnection.js
+++ Backend/src/mlConnection.js
@@ -1,9 +1,8 @@
-// mlConnection.js
 const axios = require('axios');
 
 const predictUsingMLModel = async (inputData) => {
   try {
-    const response = await axios.post('YOUR_CLOUD_RUN_MODEL_ENDPOINT', inputData);
+    const response = await axios.post('CLOUD_RUN_MODEL_ENDPOINT', inputData);
     return response.data;
   } catch (error) {
     console.error('Error predicting using ML model:', error);
