185c3589ed7aa94ed12921c4e076d5b77c1ee8a7
Madan <141886991+madan6557@users.noreply.github.com>
2023-12-12 15:42:42 +0800

    Delete Backend directory

--- Backend/src/gcs.js
+++
@@ -1,8 +0,0 @@
-const { Storage } = require('@google-cloud/storage');
-
-const storage = new Storage({
-  projectId: 'project-id', 
-  keyFilename: 'path/to/keyfile.json',
-});
-
-module.exports = storage;
